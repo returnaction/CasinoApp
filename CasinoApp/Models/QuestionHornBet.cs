f11fb424bd8c66fc72b466e48d8df61ab7b1051d
returnaction <obergannv@gmail.com>
2024-02-02 02:18:45 -0500

    Add IRepositoy and Repository

---
+++ CasinoApp/Models/QuestionHornBet.cs
@@ -0,0 +1,9 @@
+﻿namespace CasinoApp.Models
+{
+    public class QuestionHornBet
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+    }
+}
