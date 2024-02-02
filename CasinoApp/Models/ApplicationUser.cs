58b5ae09b763ddac11be8bb3c6c1fa1cf3d80168
returnaction <obergannv@gmail.com>
2024-02-02 00:41:01 -0500

    Add project files.

---
+++ CasinoApp/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CasinoApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public DateTime DateRegistered { get; set; } = DateTime.Now;
+
+
+
+    }
+}
